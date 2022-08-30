ce4d241027d05e3f4838a196f4062926387412ba
henamdu <duweihengmr@163.com>
2022-08-30 21:28:26 +0800

    feat: vue config

--- src/main.js
+++ src/main.js
@@ -1,5 +1,18 @@
 import 'virtual:windi.css'
 import { createApp } from 'vue'
+import { setupNaive } from './utils/naive'
+import router from "./router"
 import App from './App.vue'
 
-createApp(App).mount('#app')
+async function bootstrap() {
+  const app = createApp(App);
+
+  setupNaive(app);
+
+  app.use(router);
+  await router.isReady();
+
+  app.mount('#app', true);
+}
+
+void bootstrap();
