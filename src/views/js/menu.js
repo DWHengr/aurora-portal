57f73e48c0f1c7d520be21b46db2638d0fc53d98
henamdu <duweihengmr@163.com>
2022-09-18 12:07:16 +0800

    feat: add notify management menu

--- src/views/js/menu.js
+++ src/views/js/menu.js
@@ -1,6 +1,12 @@
 import { NIcon } from 'naive-ui';
 import { h } from 'vue';
-import { DocumentError20Filled, AlertOn20Filled, Settings20Filled } from '@vicons/fluent';
+import {
+  DocumentError20Filled,
+  AlertOn20Filled,
+  Settings20Filled,
+  ClockAlarm20Filled,
+  People20Filled,
+} from '@vicons/fluent';
 
 function renderIcon(icon) {
   return () => h(NIcon, null, { default: () => h(icon) });
@@ -9,7 +15,7 @@ function renderIcon(icon) {
 export const menus = [
   {
     label: '告警管理',
-    key: '',
+    key: '1',
     icon: renderIcon(Settings20Filled),
     children: [
       {
@@ -21,6 +27,18 @@ export const menus = [
         label: '告警记录',
         key: 'record',
         icon: renderIcon(DocumentError20Filled),
+      },
+    ],
+  },
+  {
+    label: '通知设置',
+    key: '2',
+    icon: renderIcon(ClockAlarm20Filled),
+    children: [
+      {
+        label: '通知用户',
+        key: 'user',
+        icon: renderIcon(People20Filled),
       },
     ],
   },
