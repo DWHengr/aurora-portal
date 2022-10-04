4effc442b0c5157e826c6e72abf53906161f01da
henamdu <duweihengmr@163.com>
2022-10-04 18:37:17 +0800

    feat: add alert silence page

---
+++ src/views/alert/alert_silence/AlertSilence.vue
@@ -0,0 +1,392 @@
+<template>
+  <div>
+    <ACard :bordered="false">
+      <div class="mb-4">
+        <n-icon :component="People20Filled" class="text-purple-800 align-middle" size="38" />
+        <h1 class="inline-block ml-2 align-middle" style="font-size: 18px">告警静默</h1>
+      </div>
+      <p> 设置告警静默信息</p>
+    </ACard>
+    <ACard class="mt-[15px]" :gradual="false">
+      <!-- 操作栏 -->
+      <div v-show="checkedRowKeys.length <= 0">
+        <n-input-group class="w-[calc(100%-5.25rem)]">
+          <AFilterSeekInput
+            class="w-full"
+            v-model:value="multipleSelectValue"
+            :seek-option="seekOption"
+          />
+          <NButton type="primary" class="w-20" @click="page"> 查找 </NButton>
+        </n-input-group>
+        <div class="inline-block">
+          <NButton
+            type="primary"
+            class="w-20 left-1"
+            @click="
+              () => {
+                (showModal = true), (is0EditAnd1Create = 1);
+              }
+            "
+          >
+            新增
+          </NButton>
+        </div>
+      </div>
+      <div v-show="checkedRowKeys.length > 0">
+        <div class="inline-block">
+          <NButton type="error" class="w-20" @click="onSilenceDeleteTip"> 删除 </NButton>
+        </div>
+      </div>
+      <!-- 数据表格 -->
+      <div class="mt-[10px]">
+        <n-data-table
+          remote
+          :row-key="(row) => row.id"
+          v-model:checked-row-keys="checkedRowKeys"
+          :on-update:checked-row-keys="
+            (keys, rows) => {
+              checkedRowKeys = keys;
+              checkedRows = rows;
+            }
+          "
+          :columns="columns"
+          :data="data"
+          :pagination="pagination"
+          :on-update:page="page"
+        />
+      </div>
+    </ACard>
+    <n-modal v-model:show="showModal" class="w-[600px]" :mask-closable="false" preset="card">
+      <template #header>
+        <div class="model-header items-center">
+          <NIcon
+            v-if="is0EditAnd1Create == 1"
+            size="26"
+            class="text-purple-800 mr-2"
+            :component="Add12Filled"
+          />
+          <NIcon
+            v-if="is0EditAnd1Create == 0"
+            size="26"
+            class="text-purple-800 mr-2"
+            :component="Edit16Filled"
+          />
+          <div v-if="is0EditAnd1Create == 1">新增告警用户</div>
+          <div v-if="is0EditAnd1Create == 0">修改告警用户</div>
+        </div>
+      </template>
+      <div class="mt-5">
+        <n-form
+          ref="formRef"
+          :model="slienceData"
+          :rules="rules"
+          :label-width="60"
+          label-placement="left"
+          :style="{
+            maxWidth: '780px',
+          }"
+        >
+          <n-form-item label="静默名称:" path="name">
+            <n-input class="w-9" v-model:value="slienceData.name" placeholder="请输入静默名称" />
+          </n-form-item>
+          <n-form-item label="静默类型:" path="type">
+            <n-input class="w-9" v-model:value="slienceData.type" placeholder="请输入静默类型" />
+          </n-form-item>
+          <n-form-item label="静默开始时间:" path="email">
+            <n-input
+              class="w-9"
+              v-model:value="slienceData.start_time"
+              placeholder="请输入静默开始时间"
+            />
+          </n-form-item>
+          <n-form-item label="静默结束时间:" path="phone">
+            <n-input
+              class="w-9"
+              v-model:value="slienceData.end_time"
+              placeholder="请输入静默结束时间"
+            />
+          </n-form-item>
+          <n-form-item label="备注:" path="phone">
+            <n-input class="w-9" v-model:value="slienceData.description" placeholder="请输入备注" />
+          </n-form-item>
+        </n-form>
+      </div>
+      <template #action>
+        <div class="flex justify-end w-full">
+          <NButton class="w-20 m-1" @click="showModal = false"> 取消 </NButton>
+          <NButton
+            v-if="is0EditAnd1Create == 1"
+            type="primary"
+            class="w-20 m-1"
+            @click="onSilenceCreate"
+          >
+            确定
+          </NButton>
+          <NButton
+            v-if="is0EditAnd1Create == 0"
+            type="primary"
+            class="w-20 m-1"
+            @click="onSilenceEdit"
+          >
+            确定
+          </NButton>
+        </div>
+      </template>
+    </n-modal>
+  </div>
+</template>
+
+<script>
+  import { h, defineComponent, ref, onMounted, reactive } from 'vue';
+  import { NButton, NIcon } from 'naive-ui';
+  import { People20Filled, Add12Filled, Delete24Regular, Edit16Filled } from '@vicons/fluent';
+  import AFilterSeekInput from '@/components/AFilterSeekInput.vue';
+  import ACard from '@/components/ACard.vue';
+  import { useRouter } from 'vue-router';
+  import silenceapi from '@/api/silence.js';
+  import { useDialog } from 'naive-ui';
+
+  const createSeekOptions = () => {
+    return [
+      {
+        label: '静默名称',
+        key: 'name',
+      },
+      {
+        label: '静默类型',
+        key: 'type',
+      },
+    ];
+  };
+
+  const rules = {
+    name: {
+      required: true,
+      message: '请输入静默名称',
+      trigger: ['input', 'blur'],
+    },
+    type: {
+      trigger: ['input'],
+      message: '请选择静默类型',
+      validator: (rule, value) => {
+        if (value) return /^1[345678]\d{9}$/.test(value);
+      },
+    },
+  };
+
+  export default defineComponent({
+    name: 'AlertSilence',
+    components: { AFilterSeekInput, ACard },
+    setup() {
+      const dialog = useDialog();
+      const multipleSelectValue = ref([]);
+      const seekSelect = ref(null);
+      const router = useRouter();
+      const checkedRowKeys = ref([]);
+      const checkedRows = ref([]);
+      const data = ref([]);
+      const showModal = ref(false);
+      const silenceData = ref({});
+      const formRef = ref(null);
+      const pagination = reactive({
+        pageSize: 10,
+      });
+      const is0EditAnd1Create = ref(0);
+      const columns = [
+        {
+          type: 'selection',
+        },
+        {
+          title: '静默名称',
+          key: 'name',
+        },
+        {
+          title: '静默类型',
+          key: 'type',
+        },
+        {
+          title: '静默开始时间',
+          key: 'start_time',
+        },
+        {
+          title: '静默结束时间',
+          key: 'end_time',
+        },
+        {
+          title: '备注',
+          key: 'description',
+        },
+        {
+          title: '操作',
+          key: 'actions',
+          render(row) {
+            return [
+              h(
+                NButton,
+                {
+                  type: 'primary',
+                  size: 'small',
+                  style: { margin: '2px' },
+                  onClick: () => {
+                    showModal.value = true;
+                    is0EditAnd1Create.value = 0;
+                    silenceData.value = { ...row };
+                  },
+                },
+                [
+                  h(NIcon, { component: Edit16Filled, size: 18, style: { marginRight: '2px' } }),
+                  '编辑',
+                ]
+              ),
+              h(
+                NButton,
+                {
+                  type: 'error',
+                  size: 'small',
+                  style: { margin: '2px' },
+                  onClick: () => onSilenceDeleteTip(row),
+                },
+                [
+                  h(NIcon, { component: Delete24Regular, size: 18, style: { marginRight: '2px' } }),
+                  '删除',
+                ]
+              ),
+            ];
+          },
+        },
+      ];
+
+      onMounted(() => {
+        page();
+      });
+
+      const page = (pagedata) => {
+        silenceapi
+          .page({
+            page: pagedata && typeof pagedata === 'number' ? pagedata : 1,
+            size: pagination.pageSize,
+            filters: multipleSelectValue.value,
+          })
+          .then((res) => {
+            if (res.code == 0) {
+              data.value = res.data.dataList;
+              pagination.itemCount = res.data.total;
+              pagination.pageCount = Math.ceil(res.data.total / pagination.pageSize);
+            }
+          });
+      };
+
+      const onSilenceCreate = () => {
+        formRef.value?.validate((errors) => {
+          if (!errors) {
+            silenceapi.create(silenceData.value).then((res) => {
+              if (res.code == 0) {
+                page();
+                formRef.value.restoreValidation();
+                silenceData.value = {};
+                showModal.value = false;
+                window.$message.success('添加成功');
+              }
+            });
+          }
+        });
+      };
+
+      const onSilenceEdit = () => {
+        silenceapi.update(silenceData.value).then((res) => {
+          if (res.code == 0) {
+            page();
+            formRef.value.restoreValidation();
+            silenceData.value = {};
+            showModal.value = false;
+            window.$message.success('修改成功');
+          }
+        });
+      };
+
+      const onSilenceDeleteTip = (row) => {
+        // eslint-disable-next-line no-console
+        console.log(checkedRows.value);
+        let ids = [];
+        let names = [];
+        if (row.id) {
+          checkedRows.value = [];
+          checkedRowKeys.value = [];
+          ids.push(row.id);
+          names.push(row.name);
+        }
+        ids.push(...checkedRowKeys.value);
+        for (let index = 0; index < checkedRows.value.length; index++) {
+          names.push(checkedRows.value[index].name);
+        }
+        dialog.warning({
+          title: '删除警告',
+          content: () => {
+            return h('lable', {}, [
+              '确定删除 ',
+              h('lable', { style: { color: '#8b5cf6', 'font-weight': 'bold' } }, names.join('、')),
+              ' ?',
+            ]);
+          },
+          positiveText: '确定',
+          negativeText: '取消',
+          onPositiveClick: () => {
+            onSilenceDelete(ids);
+          },
+        });
+      };
+
+      const onSilenceDelete = (ids) => {
+        if (ids.length > 0) {
+          silenceapi.deletes(ids).then((res) => {
+            if (res.code == 0) {
+              page();
+              checkedRowKeys.value = [];
+              window.$message.success('删除成功');
+            }
+          });
+        }
+      };
+
+      const onSeek = () => {
+        // eslint-disable-next-line no-console
+        console.log(multipleSelectValue.value);
+      };
+
+      return {
+        seekOption: createSeekOptions(),
+        columns,
+        data,
+        pagination,
+        multipleSelectValue,
+        seekSelect,
+        checkedRowKeys,
+        checkedRows,
+        is0EditAnd1Create,
+        showModal,
+        silenceData,
+        rules,
+        formRef,
+        onSilenceCreate,
+        onSilenceDeleteTip,
+        onSilenceDelete,
+        page,
+        onSeek,
+        onSilenceEdit,
+        People20Filled,
+        Add12Filled,
+        Delete24Regular,
+        Edit16Filled,
+      };
+    },
+  });
+</script>
+
+<style lang="less">
+  .model-header {
+    display: flex;
+    color: black;
+    font-size: 20px;
+    background: linear-gradient(to right, white, #c4b5fd);
+  }
+</style>
