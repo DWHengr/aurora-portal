5d6aa95f1f9389a948be26cf864ae653c0966cde
henamdu <duweihengmr@163.com>
2022-10-04 18:35:40 +0800

    feat: add alert silence api

---
+++ src/api/silence.js
@@ -0,0 +1,16 @@
+import Http from '@/utils/axios';
+
+export default {
+  page(param) {
+    return Http.post('/api/v1/silence/page', param);
+  },
+  create(param) {
+    return Http.post('/api/v1/silence/create', param);
+  },
+  deletes(param) {
+    return Http.post('/api/v1/silence/deletes', param);
+  },
+  update(param) {
+    return Http.post('/api/v1/silence/update', param);
+  },
+};
